50525827d0e34ffd1906dfc093567cd9b0d8b4ae
Alex Beyderman <Alex.Beyderman@quest.com>
2019-05-09 07:41:30 -0400

    add

--- Part3/ProcessVideoStarter/Function1.cs
+++ Part3/ProcessVideoStarter/Function1.cs
@@ -7,14 +7,15 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace ProcessVideoStarter
 {
     public static class Function1
     {
         [FunctionName("ProcessVideoStarter")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
 			[OrchestrationClient] DurableOrchestrationClient starter,
 			ILogger log)
         {
